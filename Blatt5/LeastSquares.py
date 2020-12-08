e19f14e384764461477e2ed000c94f302fa07a91
Felix Lehmann <felix.lehmann18@gmail.com>
2020-12-08 15:14:18 +0100

    A2,A4

--- Blatt5/LeastSquares.py
+++ Blatt5/LeastSquares.py
@@ -5,27 +5,42 @@ def QR(A):
     """Given a matrix A with full column rank this function uses the classical 
        Gram-Schmidt algorithm to compute a QR decomposition. It returns a tuple 
        (Q,R) of np.matrix objects with Q having shape identical to A and Q*R=A."""
-    m,n= A.shape 
-    A= A.copy() 
-    Q= np.zeros((m,n)) 
-    R= np.zeros((n,n)) 
+    m, n = A.shape
+    A = A.copy()
+    Q = np.zeros((m, n))
+    R = np.zeros((n, n))
 
-    for k in range(n): 
-        R[k,k]= np.linalg.norm(A[:,k:k+1].reshape(-1),2) 
-        Q[:,k:k+1]= A[:,k:k+1]/R[k,k]
-        R[k:k+1,k+1:n+1]= np.dot(Q[:,k:k+1].T, A[:,k+1:n+1]) 
-        A[:,k+1:n+1]= A[:, k+1:n+1] - np.dot(Q[:,k:k+1], R[k:k+1,k+1:n+1]) 
+    for k in range(n):
+        R[k, k] = np.linalg.norm(A[:, k:k+1].reshape(-1), 2)
+        Q[:, k:k+1] = A[:, k:k+1]/R[k, k]
+        R[k:k+1, k+1:n+1] = np.dot(Q[:, k:k+1].T, A[:, k+1:n+1])
+        A[:, k+1:n+1] = A[:, k+1:n+1] - np.dot(Q[:, k:k+1], R[k:k+1, k+1:n+1])
 
     return np.asmatrix(Q), np.asmatrix(R)
 
-def BackSubstitution(R, y):
+
+def BackSubstitution(R: np.matrix, y: np.ndarray):
     """Given a square upper triangular matrix R and a vector y of same size this 
        function solves R*x=y using backward substitution and returns x."""
+    m, n = R.shape
+    x = np.zeros_like(y)
+
+    for i in range(n):
+        x[i] = y[i] / R[i, i]
+        y[1:i-1] = y[1:i-1] - R[1:i-1, i] * x[i]
+
+    return x
 
 
 def LeastSquares(A, b):
     """Given a matrix A and a vector b this function solves the least squares 
        problem of minimizing |A*x-b| and returns the optimal x."""
+    Q, R = QR(A)
+
+    # Wenn ich das nachfolgende zurueckgeben soll brauche ich doch gar keine BackSubstitution?
+    x = np.linalg.inv(R) * Q.H * b
+
+    return x
 
 
 if(__name__ == "__main__"):
